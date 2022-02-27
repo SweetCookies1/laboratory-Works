174850eec7f76d74568a9f3c5db105eab98ee6cb
SweetCookies1 <andreikovalev233@gmail.com>
2022-02-27 15:38:22 +0300

    first

---
+++ cmake-build-debug/CMakeFiles/new_projects.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/new_projects.dir/main.c.obj"
+  "libnew_projects.dll.a"
+  "new_projects.exe"
+  "new_projects.exe.manifest"
+  "new_projects.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/new_projects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
